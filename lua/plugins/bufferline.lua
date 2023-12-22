b07e126ad0ce577007a300ff8fa51926c47fd56a
funkycode <thezogg@gmail.com>
2023-12-22 17:59:45 +0200

    exclude file neo-tree from some plugins

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,5 +3,9 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   opts = {
+    options = {
+      separator_style = "thin",
+      offsets = {{filetype = "neo-tree", text = "File Explorer", text_align = "center"}},
+    },
   },
 }
