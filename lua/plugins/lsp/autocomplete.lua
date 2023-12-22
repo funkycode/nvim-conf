91c8d095d86ce88ffb56bff74bb4509fb6424867
funkycode <thezogg@gmail.com>
2023-12-22 19:25:00 +0200

    split lsp and autocomplete

---
+++ lua/plugins/lsp/autocomplete.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").load {
+        exclude = { "javascript" },
+      }
+    end
+  },
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require'cmp'
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+            { name = 'buffer' },
+          })
+      })
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+            { name = 'buffer' },
+          })
+      })
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      require('lspconfig')['gopls'].setup {
+        capabilities = capabilities
+      }
+
+    end,
+  },
+}
