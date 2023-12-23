9c707cfbaf8cdf1e560460e4691cd27bd56ef1c1
funkycode <thezogg@gmail.com>
2023-12-23 12:53:17 +0200

    add cheatsheet

---
+++ lua/plugins/cheatsheet.lua
@@ -0,0 +1,9 @@
+return {
+  "sudormrfbin/cheatsheet.nvim",
+  opts = {
+    bundled_cheatsheets = true,
+    bundled_plugin_cheatsheets = true,
+    include_only_installed_plugins = true,
+  },
+}
+
