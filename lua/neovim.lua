43b037ae7fb0bae728476daa4154b20d86357835
funkycode <thezogg@gmail.com>
2023-12-23 12:54:12 +0200

    make diff more pretty

--- lua/neovim.lua
+++ lua/neovim.lua
@@ -3,3 +3,6 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 -- vim.opt.numberwidth = 6
 vim.opt.termguicolors = true
+-- show more pretty diff
+vim.opt.fillchars = vim.opt.fillchars + 'diff:.'
+
