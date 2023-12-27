864a952c5fc9db31018dd3346d9ef58dbd9ec128
funkycode <thezogg@gmail.com>
2023-12-27 20:58:55 +0200

    add zoxide and add some extensions to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,14 +14,41 @@ return  {
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
+      local z_utils = require("telescope._extensions.zoxide.utils")
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {}
-          }
+          },
+          persisted = {
+            persisted = {
+              layout_config = { width = 0.55, height = 0.55 }
+            }
+          },
+          zoxide = {
+            prompt_title = "[ Walking on the shoulders of TJ ]",
+            mappings = {
+              default = {
+                after_action = function(selection)
+                  print("Update to (" .. selection.z_score .. ") " .. selection.path)
+                end
+              },
+              -- ["<C-s>"] = {
+              --   before_action = function(selection)
+              --     print("before C-s")
+              --   end,
+              --   action = function(selection)
+              --     vim.cmd.edit(selection.path)
+              --   end
+              -- },
+              -- ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+            },
+          },
         }
       })
       require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("persisted")
+      require("telescope").load_extension('zoxide')
     end,
   },
 }
