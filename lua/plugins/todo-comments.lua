40aa66b7e75967fdb7c86009f88161e5fed74f03
funkycode <thezogg@gmail.com>
2023-12-28 00:52:22 +0200

    add alias for FIXME

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,10 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    keywords = {
+      FIX = {
+        alt = { "FIXME", "FIXIT" },
+      },
+    },
   },
 }
