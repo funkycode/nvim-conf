f3a0550df88e5e0f39063489c3710b53956763d0
funkycode <thezogg@gmail.com>
2024-01-06 11:50:29 +0200

    add system clipboard

--- lua/neovim.lua
+++ lua/neovim.lua
@@ -4,5 +4,6 @@ vim.opt.signcolumn = "number"
 -- vim.opt.numberwidth = 6
 vim.opt.termguicolors = true
 -- show more pretty diff
-vim.opt.fillchars = vim.opt.fillchars + 'diff:.'
-
+vim.opt.fillchars = vim.opt.fillchars + "diff:."
+-- use system clipboard
+vim.opt.clipboard = "unnamedplus"
