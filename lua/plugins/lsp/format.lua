31e60b675d1bb58fb244c03a2413af486b494418
funkycode <thezogg@gmail.com>
2024-01-06 11:07:36 +0200

    move keys to lazy and add description

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -36,25 +36,50 @@ return {
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {},
-		config = function()
-			vim.keymap.set("n", "<leader>xx", function()
-				require("trouble").toggle()
-			end)
-			vim.keymap.set("n", "<leader>xw", function()
-				require("trouble").toggle("workspace_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xd", function()
-				require("trouble").toggle("document_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xq", function()
-				require("trouble").toggle("quickfix")
-			end)
-			vim.keymap.set("n", "<leader>xl", function()
-				require("trouble").toggle("loclist")
-			end)
-			vim.keymap.set("n", "gR", function()
-				require("trouble").toggle("lsp_references")
-			end)
-		end,
+		keys = {
+
+			{
+				"<leader>xx",
+				function()
+					require("trouble").toggle()
+				end,
+				desc = "Trouble toggle",
+			},
+			{
+				"<leader>xw",
+				function()
+					require("trouble").toggle("workspace_diagnostics")
+				end,
+				desc = "Trouble toggle workspace diagnostics",
+			},
+			{
+				"<leader>xd",
+				function()
+					require("trouble").toggle("document_diagnostics")
+				end,
+				desc = "Trouble toggle document diagnostics",
+			},
+			{
+				"<leader>xq",
+				function()
+					require("trouble").toggle("quickfix")
+				end,
+				desc = "Trouble toggle quickfix",
+			},
+			{
+				"<leader>xl",
+				function()
+					require("trouble").toggle("loclist")
+				end,
+				desc = "Trouble toggle loclist",
+			},
+			{
+				"gR",
+				function()
+					require("trouble").toggle("lsp_references")
+				end,
+				desc = "Trouble toggle LSP references",
+			},
+		},
 	},
 }
