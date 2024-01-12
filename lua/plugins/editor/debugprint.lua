069bcee758aae6a907e401be010ea2571f834009
funkycode <thezogg@gmail.com>
2024-01-12 14:23:17 +0200

    add keys for debug print

--- lua/plugins/editor/debugprint.lua
+++ lua/plugins/editor/debugprint.lua
@@ -1,4 +1,48 @@
 return {
 	"andrewferrier/debugprint.nvim",
-	opts = {},
+	opts = {
+		create_keymaps = false,
+		create_commands = false,
+	},
+	keys = {
+		{
+			"<leader>dpa",
+			function()
+				return require("debugprint").debugprint()
+			end,
+			expr = true,
+			desc = "[printdebug] add debug line after",
+		},
+		{
+			"<leader>dpb",
+			function()
+				return require("debugprint").debugprint({ above = true })
+			end,
+			expr = true,
+			desc = "[printdebug] add debug line before",
+		},
+		{
+			"<leader>dPa",
+			function()
+				return require("debugprint").debugprint({ variable = true })
+			end,
+			expr = true,
+			desc = "[printdebug] add debug line after",
+		},
+		{
+			"<leader>dPb",
+			function()
+				return require("debugprint").debugprint({ above = true, variable = true })
+			end,
+			expr = true,
+			desc = "[printdebug] add debug line before",
+		},
+		{
+			"<leader>dpc",
+			function()
+				return require("debugprint").deleteprints()
+			end,
+			desc = "[printdebug] clean all debug entries",
+		},
+	},
 }
